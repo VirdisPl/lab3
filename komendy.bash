fa0bb64fc8858d701ab7b7c6a98f8353720349c6
VirdisPl <35819544+VirdisPl@users.noreply.github.com>
2024-07-06 14:25:27 +0200

    Update and rename skryptlab4 to komendy.bash

---
+++ komendy.bash
@@ -0,0 +1,14 @@
+
+git init
+git add <plik>
+git commit -m "Opis commitu"
+git branch <nazwa_gałęzi>
+git checkout <nazwa_gałęzi>
+git merge <nazwa_gałęzi>
+git branch -d <nazwa_gałęzi>
+git tag <nazwa_tag>
+git log
+git status
+git diff
+git rebase <nazwa_gałęzi>
+git branch
