003c7684fb6f1e739bcd1d6116b0a715b76e2b00
VirdisPl <35819544+VirdisPl@users.noreply.github.com>
2024-07-06 14:26:39 +0200

    Update komendy.sh

--- komendy.sh
+++ komendy.sh
@@ -12,3 +12,12 @@ git status
 git diff
 git rebase <nazwa_gałęzi>
 git branch
+git config --global user.email "email@dsw.edu.pl"
+git config --global user.name "Twoje Imię"
+git init
+git add commands.sh
+git commit -m "Dodano skrypt z komendami z tutorialu"
+git remote add origin https://github.com/TwójLogin/nazwa-repozytorium.git
+git branch -M main
+git push -u origin main
+git push
